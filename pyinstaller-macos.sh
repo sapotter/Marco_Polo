00c3c72fe7ba6005dbfb360753cc17da571d2bd8
Stephen A Potter <potter@hwi.buffalo.edu>
2022-03-12 14:35:56 -0500

    Check for various errors, i.e., is the conda env "polo" activated.

--- pyinstaller-macos.sh
+++ pyinstaller-macos.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
- pyinstaller --collect-all tensorflow \
- --hidden-import pptx \
- --add-data src/data:data --add-data src/astor:astor \
- --add-data src/unrar:unrar --add-data src/templates:templates \
- --icon ~/Marco_Polo/icon.icns \
- --clean \
- -y \
- --name polox src/Polo.py
+# py2app borked on missing libpython3.7m.dylib. Even this fix worked
+# but the build failed regardless.
+
+# pyinstall is quite happy without it.
+
+#POLO_ENV=/usr/local/Caskroom/miniconda/base/envs/polo
+#
+#if [ ! -e $POLO_ENV/lib/libpython3.7.a ]; then
+#  pushd $POLO_ENV/lib && ln -s libpython3.7m.a libpython3.7.a && popd || exit 1;
+#fi
+#if [ ! -e $POLO_ENV/lib/libpython3.7.dylib ]; then
+#  pushd $POLO_ENV/lib && ln -s libpython3.7m.dylib libpython3.7.dylib && popd || exit 1;
+#fi
+
+declare -i errors=0
+if [ "$(basename "$(pwd)")" != "Marco_Polo" ]; then
+    echo "ERROR: pyinstaller-macos.sh must be run in the Marco_Polo project root directory."
+    _=$((errors++))
+fi
+if [ "${CONDA_DEFAULT_ENV:-notpolo}" = "notpolo" ]; then
+    echo "ERROR: activate required conda environment with : conda activate polo"
+    _=$((errors++))
+fi
+[ $((errors)) -gt 0 ] && exit 1
+PYINSTALLER_INSTALLED=$(conda list -c pyinstaller | awk '/pypi\/pypi::pyinstaller-[[:digit:]]/ { print $0 }')
+if [ "${PYINSTALLER_INSTALLED}" ]; then
+    echo "Install pyinstaller with: pip install pyinstaller"
+    exit 1
+fi
+
+pyinstaller --collect-all tensorflow \
+--hidden-import pptx \
+--add-data ../src/data:data --add-data ../src/astor:astor \
+--add-data ../src/unrar:unrar --add-data ../src/templates:templates \
+--icon ../macOS/appplication.icns \
+--clean \
+-y \
+--specpath macOS \
+src/Polo.py
