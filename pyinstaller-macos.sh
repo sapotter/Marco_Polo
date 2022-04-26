8ad5f99730f87d5ecd6762074885540afb1afaed
Stephen A Potter <potter@hwi.buffalo.edu>
2022-04-26 16:43:19 -0400

    An enhanced version: use -h to see usage help.

--- pyinstaller-macos.sh
+++ pyinstaller-macos.sh
@@ -1,5 +1,79 @@
 #!/bin/bash
 
+# STYLE_COLOR
+RED="\033[0;31m"
+LIGHT_GREEN="\033[1;32m"
+NC="\033[0m" # No Color
+BOLD="\033[1m"
+
+OS_NAME=$(uname)
+
+usage() {
+    cat <<EOT
+$(echo -e ${BOLD}NAME${NC})
+    $(basename "$0") -- Create macOS App bundle
+
+$(echo -e ${BOLD}SYNOPSIS${NC})
+    $(basename "$0") [-m] [-b | -d | -o | -w] [-s [spec-path]] [-h]
+
+$(echo -e ${BOLD}OPTIONS${NC})
+    -m Run py-makespec. Default: pyinstaller.
+    -b Specify the macOS CFBundleIdentifier if an application bundle
+       is created. Default: org.hauptman-woodward.polo.
+    -d Create a directory containing tha application and its supporting
+       components. Default.
+    -o Create a single file application.
+    -w Create a macOS application bundle.
+    -s Specify the "spec" file directory path. Default: ./macOS.
+    -h Help.
+
+$(echo -e ${BOLD}Examples${NC})
+EOT
+}
+
+# shellcheck disable=SC2223
+: ${RUNNER:=pyinstaller --clean -y}
+# shellcheck disable=SC2223
+: ${SPEC_PATH:=macOS}
+# shellcheck disable=SC2223
+: ${BUNDLE_ID:=org.hauptman-woodward.polo}
+
+
+while getopts "bdmows:h" option; do
+    case $option in
+        b)
+            bundle="--osx-bundle-identifier ${BUNDLE_ID}"
+            ;;
+        d)
+            onedir="--onedir"
+            ;;
+        m)
+            RUNNER="pyi-makespec"
+            ;;
+        s)
+            SPEC_PATH=$OPTARG
+            ;;
+        o)
+            onefile="--onefile"
+            ;;
+        w)
+            windowed="--windowed"
+            ;;
+        h|*)
+            usage
+            exit 1
+    esac
+done
+shift "$((OPTIND - 1))"
+
+if [ "$makespec" ]; then RUNNER="pyi-makespec"; fi
+
+CMD="$RUNNER $bundle $onedir $onefile $windowed --specpath ${SPEC_PATH}"
+# Trim extra spaces
+CMD=$(echo "$CMD" | \
+    awk '{ gsub(/[[:space:]]{2,}/, " "); print $0; }' | \
+    awk '{ gsub(/^[[:space:]]+|[[:space:]]+$/, ""); print $0; }')
+
 # py2app borked on missing libpython3.7m.dylib. Even this fix worked
 # but the build failed regardless.
 
@@ -30,12 +104,9 @@ if [ "${PYINSTALLER_INSTALLED:-notinstalled}" = "notinstalled" ]; then
     exit 1
 fi
 
-pyinstaller --collect-all tensorflow \
+$CMD --collect-all tensorflow \
 --hidden-import pptx \
 --add-data ../src/data:data --add-data ../src/astor:astor \
 --add-data ../src/unrar:unrar --add-data ../src/templates:templates \
---icon ../macOS/appplication.icns \
---clean \
--y \
---specpath macOS \
+--icon ../macOS/application.icns \
 src/Polo.py
