1c5771238d9f44d42c08850cf701e4978ecd7d75
Stephen A Potter <sapotter@buffalo.edu>
2026-01-08 23:29:54 -0500

    Adds device listing for TensorFlow GPU test
    Adds a print statement to list available devices in the TensorFlow GPU test script.
    
    This change helps diagnose issues related to GPU availability by providing a clear output of recognized devices.

--- test/GPU-test-tf.py
+++ test/GPU-test-tf.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 print("TensorFlow version:", tf.__version__)
+print("Devices:", tf.config.list_physical_devices())
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
     print("GPU is available:", gpus)
